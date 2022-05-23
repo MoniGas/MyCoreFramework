8b40bee507f4c579b2df6dc4da6f4723c34020a5
renre <renre@DESKTOP-VG46T7G>
2022-05-23 22:41:43 +0800

    添加项目文件。

---
+++ MyCoreFramework/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using MCF.Data.Orm;
+using MCF.Model;
+using Microsoft.AspNetCore.Mvc;
+using MyCoreFramework.Models;
+using System.Data.SqlClient;
+using System.Diagnostics;
+
+namespace MyCoreFramework.Controllers
+{
+    public class HomeController : Controller
+    {
+        IOrmDbEntity db=DbVisit.GetDbEntity();
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            int id = 17616;
+            var model = db.Entity<BBS_User>(" and Id=@id ", new SqlParameter("@id", id));
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MyCoreFramework/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyCoreFramework.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
